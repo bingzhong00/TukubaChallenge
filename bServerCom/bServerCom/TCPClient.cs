7d88c36bd68e85bdfdb460e1a4575eadc2f4ae53
terakenxx <terakenxx@hotmail.co.jp>
2016-11-01 22:21:36 +0900

    bServerCom追加

---
+++ bServerCom/bServerCom/TCPClient.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CersioIO
+{
+    public class TCPClient : IDisposable
+    {
+
+        private string ipString;//ListenするIPアドレス
+
+        public string ipStringProperty
+        {
+            get { return ipString; }
+            set { ipString = value; }
+        }
+
+        private int port;//Listenするポート番号
+
+        public int portProperty
+        {
+            get { return port; }
+            set { port = value; }
+        }
+
+        private System.Net.Sockets.NetworkStream ns;
+
+        public System.Net.Sockets.NetworkStream MyProperty
+        {
+            get { return ns; }
+        }
+
+        private System.Net.Sockets.TcpClient objSck;
+
+        public System.Net.Sockets.TcpClient SckProperty
+        {
+            get { return objSck; }
+        }
+
+
+        // 接続フラグ
+        public bool ConnectResult = false;
+
+        public TCPClient(string ipAddr, int ipPort )
+        {
+            ipString = ipAddr;
+            port = ipPort;
+            ConnectResult = false;
+        }
+
+        public TCPClient() : this("192.168.1.1",50001)
+        {
+        }
+
+        //~TCPClient()
+        public void Dispose()
+        {
+            DisConnect();
+        }
+
+        /// <summary>
+        /// 切断
+        /// </summary>
+        public void DisConnect()
+        {
+            if (ns != null)
+            {
+                try
+                {
+                    ns.Close();
+                }
+                catch { }
+                ns = null;
+            }
+            if (objSck != null)
+            {
+                try
+                {
+                    objSck.Close();
+                }
+                catch { }
+                objSck = null;
+            }
+				
+            ConnectResult = false;
+        }
+
+        public bool Start()
+        {
+            System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(this.ipString);
+
+            if (objSck != null)
+            {
+                objSck.Close();
+                objSck = null;
+            }
+            
+            objSck = new System.Net.Sockets.TcpClient();
+
+            try
+            {
+                objSck.Connect(ipAdd, port);
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+            //catch (Exception)
+            //{
+                
+            //    throw;
+            //}
+            
+
+            //NetworkStreamを取得
+            ns = objSck.GetStream();
+
+            ConnectResult = true;
+            return ConnectResult;
+
+        }
+
+
+    }
+}
